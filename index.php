066d028e2d6861730e136d1205d81be50d2b1f8f
Janine Hempy <jhempy@gmail.com>
2017-10-26 16:39:24 -0400

    WIP - still testing

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php  
 
-  // var_dump($_SERVER);
+// var_dump(substr($_SERVER['HTTP_HOST'], 0, 9));
+
+//   if (substr($_SERVER['HTTP_HOST'], 0, 9) == 'localhost') {
+//     $url = $_SERVER['HTTP_HOST'] . '/app';
+//   }
+//   else {
+//     $url = '/app';
+//   }
+  
+// var_dump($url);
 
   $url = $_SERVER['HTTP_HOST'] . '/app';
-  header("Location: " . $url);
+  header("Location: http://" . $url);
   die();
 ?>
