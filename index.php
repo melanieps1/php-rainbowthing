5aebce9306666a5bb40364151088bd2dd5952b48
Janine Hempy <jhempy@gmail.com>
2017-10-26 16:26:02 -0400

    Troubleshooting Heroku

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php  
-  $url = $_SERVER['HTTP_HOST'] . '/app';
-  header("Location: " . $url);
-  die();
+
+  var_dump($_SERVER);
+
+  // $url = $_SERVER['HTTP_HOST'] . '/app';
+  // header("Location: " . $url);
+  // die();
 ?>
